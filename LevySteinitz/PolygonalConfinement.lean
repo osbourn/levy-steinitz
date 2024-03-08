96f17179e123a0f5735b5c83f3f4e4519b9eac3c
Niels Voss <code@nielsvoss.com>
2024-03-07 23:18:39 -0800

    Declare maximal_indicies and maximal_vector

--- LevySteinitz/PolygonalConfinement.lean
+++ LevySteinitz/PolygonalConfinement.lean
@@ -14,18 +14,35 @@ lemma polygonalConstantOne : polygonalConstant 1 = 1 := by
   unfold polygonalConstant
   norm_num
 
-section
-
+/-
 variable {m : ℕ} (hm : 0 < m) (v : Fin m → ℝ)
 
 theorem linear_polygonal_confinement_theorem {m : ℕ} (hm : 0 < m) (v : Fin m → ℝ)
   (hv₁ : ∑ i : Fin m, v i = 0) (hv₂ : ∀ i : Fin m, ‖v i‖ ≤ 1) :
   ∃ P : Equiv.Perm (Fin m), P ⟨0, hm⟩ = ⟨0, hm⟩ ∧
   ∀ j : Fin m, ‖∑ i in Finset.Iic j, v i‖ ≤ 1 := sorry
+-/
 
-end
+variable {n : ℕ} {m : ℕ} {hm : 0 < m} (v : Fin m → EuclideanSpace ℝ (Fin n))
 
-variable (n : ℕ) {m : ℕ} (hm : 0 < m) (v : Fin m → EuclideanSpace ℝ (Fin n))
+abbrev sum_indicies (s : Finset (Fin m)) : EuclideanSpace ℝ (Fin n) := ∑ i : s, v i
+
+noncomputable def maximal_indicies_aux : Option (Finset (Fin m)) :=
+  (Finset.univ.powerset : Finset (Finset (Fin m))).toList.argmax (‖sum_indicies v ·‖)
+
+lemma maximal_indicies_aux_isSome : (maximal_indicies_aux v).isSome = true := by
+  by_contra h
+  rw [Option.not_isSome_iff_eq_none] at h
+  unfold maximal_indicies_aux at h
+  rw [List.argmax_eq_none] at h
+  apply Finset.Nonempty.toList_ne_nil _ h
+  exact Finset.powerset_nonempty _
+
+noncomputable def maximal_indicies : Finset (Fin m) :=
+  Option.get (maximal_indicies_aux v) (maximal_indicies_aux_isSome v)
+
+noncomputable def maximal_vector : EuclideanSpace ℝ (Fin n) :=
+  sum_indicies v (maximal_indicies v)
 
 theorem polygonal_confinement_theorem
   (hv₁ : ∑ i : Fin m, v i = 0) (hv₂ : ∀ i : Fin m, ‖v i‖ ≤ 1) :
